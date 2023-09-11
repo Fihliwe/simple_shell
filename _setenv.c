659fc5ad8bf727e4512d0b72754cf47450d88ef4
Fihliwe <siyabulelmncube@gmail.com>
2023-09-11 01:15:20 -0700

    setenv file works, do not change anything

---
+++ _setenv.c
@@ -0,0 +1,73 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _setenv -  Function to change or add an environment variable.
+ * @name: constant character
+ * @value: constant character
+ * @overwrite: looped integer value
+ * Return: -1 when updating, -1 when adding, 0 if success
+ */
+
+int _setenv(const char *name, const char *value, int overwrite)
+{
+	char *exists;
+	char *n_env;
+
+	exists = getenv(name);
+
+	if (exists)
+	{
+		if (overwrite)
+		{
+			n_env = (char *)malloc(strlen(name) + strlen(value) + 2);
+			if (!n_env)
+			{
+				return (-1);
+			}
+			sprintf("%s=%s", n_env, name, value);
+
+			if (putenv(n_env) != 0)
+			{
+				return (-1);
+			}
+		}
+	}
+
+	else
+	{
+		if (setenv(name, value, 1) != 0)
+		{
+			return (-1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * main - to test output
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *var;
+
+	var = getenv("Variable");
+
+	_setenv("Variable", "n_value", 1);
+
+	if (var)
+	{
+		printf("Variable = %s\n", var);
+	}
+
+	else
+	{
+		printf("Variable is not in the environment\n");
+	}
+	return (0);
+}
--- shell.h
+++ shell.h
@@ -1,16 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
 
-void _variables(char *str);
+/* environment functions (enviro.c) */
+char *_getenv(const char *name);
+int _setenv(const char *name, const char *value, int overwrite);
+int _unsetenv(const char *name);
+
+/* function for variables */
+
+/* function for alias */
+
+/* function for logical operators */
+
+/* function for commands seperator */
+
+/* function for builtin command (cd - change directory) */
+
+#endif
